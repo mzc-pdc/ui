b54c6d3efa87a008d72299c36489b848834ba5db
dhmkchs <dhmkchs@gmail.com>
2023-04-14 16:18:37 +0900

    edit package peerDependencies, rollup plugin

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default [
   {
@@ -20,6 +21,7 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
