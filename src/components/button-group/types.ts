0cb41915ca4fedb9962330389128c7f1f3876439
mz-superek <superek@mz.co.kr>
2023-05-26 16:02:02 +0900

    [fix] button group : types 수정

--- src/components/button-group/types.ts
+++ src/components/button-group/types.ts
@@ -2,26 +2,5 @@ import {
     ButtonGroupProps as MuiButtonGroupProps,
 } from '@mui/material';
 
-
-export type Variant = 'contained' | 'outlined' | 'dashed';
-export type Color =
-    | 'inherit'
-    | 'primary'
-    | 'secondary'
-    | 'success'
-    | 'info'
-    | 'error'
-    | 'warning'
-    | 'grayscale'
-    | 'grayscaleSecondary'
-    | 'default'
-    | undefined;
-export type Size = 'small' | 'medium' | 'large';
-
-export interface StyledButtonGroupProps  {
-    color?: Color;
-    variant?: Variant;
-    size?: Size;
+export interface ButtonGroupProps extends MuiButtonGroupProps {
 }
-
-export type ExtendedButtonGroupProps = MuiButtonGroupProps & StyledButtonGroupProps;
