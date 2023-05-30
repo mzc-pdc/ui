cfc6d0d24c75fac031f3a19f4be4da6e862595c3
mz-superek <superek@mz.co.kr>
2023-05-30 10:25:16 +0900

    [feature] component: box 추가

---
+++ src/components/box/box.stories.ts
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Box } from './box';
+
+const meta: Meta<typeof Box> = {
+  title: 'Components / Box',
+  tags: ['autodocs'],
+  component: Box,
+  parameters: {
+    docs: {
+      description: {
+        component: 'Box Description',
+      },
+    },
+  },
+  args: {
+    //storybook common args
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default = {
+  args: {
+    children: `aaaa`,
+  },
+} satisfies Story;
