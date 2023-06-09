064c078d12f4404f824d4e0155bb7a90e143f318
mz-superek <superek@mz.co.kr>
2023-06-09 13:27:25 +0900

    [fix] modal : error 수정

--- src/components/modal/modal.stories.tsx
+++ src/components/modal/modal.stories.tsx
@@ -39,24 +39,26 @@ export const Default: Story = {
                     aria-labelledby="modal-modal-title"
                     aria-describedby="modal-modal-description"
                 >
-                    <Box sx={{
-                        position: 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                        width: 400,
-                        backgroundColor: '#fff',
-                        border: '2px solid #000',
-                        boxShadow: 24,
-                        padding: 6,
-                    }}>
-                        <Typography id="modal-modal-title" variant="h6" component="h2">
-                            Text in a modal
-                        </Typography>
-                        <Typography id="modal-modal-description" sx={{mt: 2}}>
-                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                        </Typography>
-                    </Box>
+                    <>
+                        <Box sx={{
+                            position: 'absolute',
+                            top: '50%',
+                            left: '50%',
+                            transform: 'translate(-50%, -50%)',
+                            width: 400,
+                            backgroundColor: '#fff',
+                            border: '2px solid #000',
+                            boxShadow: 24,
+                            padding: 6,
+                        }}>
+                            <Typography id="modal-modal-title" variant="h6" component="h2">
+                                Text in a modal
+                            </Typography>
+                            <Typography id="modal-modal-description" sx={{mt: 2}}>
+                                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                            </Typography>
+                        </Box>
+                    </>
                 </Modal>
             </>
         )
